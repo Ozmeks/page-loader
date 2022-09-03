f33f404befa13dfc513d687821342e40881450d4
ozmeks <ozmeks@yandex.ru>
2022-09-03 22:55:48 +0500

    add image download

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -11,13 +11,22 @@ import downloadPage from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
-const expectedResult = await readFile(getFixturePath('test.html'), 'utf-8');
+const pageBefore = await readFile(getFixturePath('page-before.html'), 'utf-8');
+const imgNode = await readFile(getFixturePath('files/nodejs.png'), 'utf-8');
+const imgLogo = await readFile(getFixturePath('files/logo.svg'), 'utf-8');
+const pageAfter = await readFile(getFixturePath('page-after.html'), 'utf-8');
 
 beforeAll(async () => {
   nock.disableNetConnect();
   nock('https://ru.hexlet.io')
     .get('/courses')
-    .reply(200, expectedResult);
+    .reply(200, pageBefore);
+  nock('https://ru.hexlet.io')
+    .get('/assets/professions/nodejs.png')
+    .reply(200, imgNode);
+  nock('https://cdn2.hexlet.io')
+    .get('/assets/logo_ru-495f05850e0095ea722a2b583565d492719579c02b0ce61d924e4f895fabf781.svg')
+    .reply(200, imgLogo);
 });
 
 let tempPath = '';
@@ -25,9 +34,9 @@ beforeEach(async () => {
   tempPath = await mkdtemp(join(os.tmpdir(), 'page-loader-'));
 });
 
-test('download simple html', async () => {
+test('download html with images', async () => {
   const url = 'https://ru.hexlet.io/courses';
   const receivedPath = await downloadPage(url, tempPath);
   const receivedResult = await readFile(receivedPath, 'utf-8');
-  expect(expectedResult).toEqual(receivedResult);
+  expect(pageAfter).toEqual(receivedResult);
 });
--- src/index.js
+++ src/index.js
@@ -1,23 +1,56 @@
 import axios from 'axios';
 import fsp from 'fs/promises';
-import { resolve } from 'path';
+import { createWriteStream } from 'fs';
+import { resolve, parse, join } from 'path';
+import { load } from 'cheerio';
+import { format } from 'prettier';
 
-const makeFileName = (href) => {
-  const url = new URL(href);
-  const { protocol } = url;
-  const fileNameWithSymbols = href.slice((`${protocol}//`).length);
-  const fileName = fileNameWithSymbols.replaceAll(/[^A-Za-z0-9]/g, '-');
-  return `${fileName}.html`;
+const makeName = (name) => name.replaceAll(/[^A-Za-z0-9]/g, '-');
+
+const makeFileName = (pathname) => {
+  const { dir, name, ext } = parse(pathname);
+  const path = makeName(resolve(dir, name));
+  return `${path}${ext}`;
 };
 
-const downloadPage = (url, path = 'default') => {
-  const fileName = makeFileName(url);
-  const outputPath = (path === 'default') ? process.cwd() : path;
-  const filePath = resolve(outputPath, fileName);
+const downloadPage = (inputUrl, inputPath = 'default') => {
+  const outputPath = (inputPath === 'default') ? process.cwd() : inputPath;
+  const { host: inputHost, pathname: inputPathName } = new URL(inputUrl);
+  const pageName = makeName(`${inputHost}${inputPathName}`);
+  const prefixFileName = makeName(inputHost);
+  const dirName = `${pageName}_files`;
+  const outputFilePath = resolve(outputPath, `${pageName}.html`);
+  const filesPath = resolve(outputPath, dirName);
 
-  const promise = axios.get(url)
-    .then(({ data }) => fsp.writeFile(filePath, data))
-    .then(() => filePath);
+  let $;
+  const promises = [];
+  const promise = axios.get(inputUrl)
+    .then(({ data }) => {
+      $ = load(data);
+      $('img').each((i, el) => {
+        const src = $(el).attr('src');
+        const { href, pathname } = new URL(src, inputUrl);
+        const fileName = `${prefixFileName}${makeFileName(pathname)}`;
+        const path = resolve(filesPath, fileName);
+        promises.push({ url: href, path });
+        $(el).attr('src', join(dirName, fileName));
+      });
+      return (promises.length > 0) ? fsp.mkdir(filesPath) : null;
+    })
+    .then(() => {
+      if (promises.length > 0) {
+        promises.map(({ url, path }) => axios
+          .get(url, { responseType: 'stream' })
+          .then(({ data }) => data.pipe(createWriteStream(path))));
+        return Promise.all(promises);
+      }
+      return null;
+    })
+    .then(() => {
+      const outputHtml = format($.html(), { parser: 'html' });
+      return fsp.writeFile(outputFilePath, outputHtml);
+    })
+    .then(() => outputFilePath);
 
   return promise;
 };
